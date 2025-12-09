c7214c78279f06cceb13c1a7e60746dc9708e152
Ahmed Mohamed <ahmed0033ahmed.mo@gmail.com>
2025-12-09 08:03:35 +0200

    start to retouch backend

--- Ecommerce_API/auth/login.php
+++ Ecommerce_API/auth/login.php
@@ -5,8 +5,10 @@ include "../connect.php";
 $email = filterRequest("email");
 $password = sha1(filterRequest("password"));
 
-$stmt = $con->prepare("SELECT * FROM users WHERE user_email = ? AND user_password = ? AND user_approve = 1");
-$stmt->execute([$email, $password]);
-$conunt = $stmt->rowCount();
+// $stmt = $con->prepare("SELECT * FROM users WHERE user_email = ? AND user_password = ? AND user_approve = 1");
+// $stmt->execute([$email, $password]);
+// $conunt = $stmt->rowCount();
 
-result($conunt); 
+// result($conunt); 
+
+getData("users" , "user_email = ? AND user_password = ? AND user_approve = 1 " , array($email , $password));
